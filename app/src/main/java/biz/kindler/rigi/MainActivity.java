8b9121be01acc223eb08b093c3a430ff7ec0ccf1
KaterMoritz <katermoritz100@gmail.com>
2019-04-05 17:01:21 +0200

    master testcommit 1

--- app/src/main/java/biz/kindler/rigi/MainActivity.java
+++ app/src/main/java/biz/kindler/rigi/MainActivity.java
@@ -159,6 +159,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
+        System.out.println( "doing something in master");
+
         // test commit
 
 /*
